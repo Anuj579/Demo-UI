e56ac8f2401f444f1ae48ddf5890a71e80eac77f
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-04-19 22:54:33 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Carousel for mobile screen
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    // autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false
+});
