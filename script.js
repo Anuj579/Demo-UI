c73979de5187d5a68baadf4e32c8b46b9f474175
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-04-19 22:59:37 +0530

    updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var owl = $('.owl-carousel');
 owl.owlCarousel({
     items:1,
     loop:true,
-    // autoplay:true,
+    autoplay:true,
     autoplayTimeout:5000,
     autoplayHoverPause:false
 });
