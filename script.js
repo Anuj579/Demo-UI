76bec6ec26cbb78113e11139b69086079a258c8b
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-04-22 18:11:08 +0530

    final updates

--- script.js
+++
@@ -1,9 +0,0 @@
-// Carousel for mobile screen
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:1,
-    loop:true,
-    autoplay:true,
-    autoplayTimeout:5000,
-    autoplayHoverPause:false
-});
