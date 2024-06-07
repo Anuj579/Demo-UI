7c762345fec68cdf3c6b7fdec3e6d3e06c2e1d03
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-06-07 10:04:12 +0530

    updates

--- js/script.js
+++ js/script.js
@@ -7,24 +7,3 @@ owl.owlCarousel({
     autoplayTimeout:5000,
     autoplayHoverPause:false
 });
-
-let scrolling = false; // Flag to track scrolling state
-let timeout; // Variable to store timeout
-
-// Function to handle scroll event
-function handleScroll(event) {
-  if (!scrolling) {
-    scrolling = true;
-    clearTimeout(timeout);
-
-    timeout = setTimeout(() => {
-      scrolling = false; // Reset scrolling flag
-      let currentTop = window.scrollY; // Get current scroll position
-      let slideIndex = Math.round(currentTop / window.innerHeight); // Calculate nearest slide index
-      let targetTop = slideIndex * window.innerHeight; //
-      window.scrollTo({ top: targetTop, behavior: 'smooth' })
-    }, 100); //
-  }
-}
-
-window.addEventListener('scroll', handleScroll);
